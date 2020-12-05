2dad7a35990aef2d870bb850eb8e53cba61626b3
jet2018 <ezrajet9@gmail.com>
2020-12-05 14:49:55 +0300

    setting up the app

---
+++ src/views/all_students.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-table striped hover :student="students" :fields="fields"></b-table>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        // Note `isActive` is left out and will not appear in the rendered table
+        fields: ['first_name', 'last_name', 'sex', 'grade'],
+        students: []
+      }
+    }
+    methods:{
+    	
+    }
+  }
+</script>
